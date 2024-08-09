980c468da60964bbfda29f12578ccfcce3407755
Sufyan <Sufyan265@users.noreply.github.com>
2024-08-09 17:06:51 +0500

    add build in vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
+  },
   // server: {
   //   host: true, // Use 0.0.0.0 to listen on all IP addresses
   //   port: 5173, // or any other port you prefer
